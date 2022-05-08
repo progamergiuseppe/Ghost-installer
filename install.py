febc9267ec57801f63d1224fbab3213178a7e801
progamergiuseppe <97746064+progamergiuseppe@users.noreply.github.com>
2022-05-08 21:29:41 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,21 +1,12 @@
 import time
 import os
-os.system("cls")
+os.system("clear")
 import sys
-os.system("pip install pynput")
 os.system("clear")
 print("Ghost Installer - By: Giuseppe\n\nGhost made by : EntySec (https://github.com/EntySec/Ghost)")
 print("")
 yes_or_no = input("Are you sure want to install?\n\nY/n: ")
 os.system("clear")
-if yes_or_no == "N" or "n":
-    os.system("clear")
-    print("Goodbye :)")
-    sys.exit()
-while yes_or_no == "":
-    os.system("clear")
-    print("Sorry, i didn't get that")
-    sys.exit()
 if yes_or_no == "Y" or "y":
     kali_or_termux = input("Are you using [KALI]/[TERMUX]: ")
     os.system("clear")
@@ -29,4 +20,12 @@ if yes_or_no == "Y" or "y":
     if kali_or_termux == "TERMUX" or "[TERMUX]":
         print("Starting download on TERMUX ...")
         time.sleep(1)
+if yes_or_no == "N" or "n":
+    os.system("clear")
+    print("Goodbye :)")
+    sys.exit()
+while yes_or_no == "":
+    os.system("clear")
+    print("Sorry, i didn't get that")
+    sys.exit()
     
